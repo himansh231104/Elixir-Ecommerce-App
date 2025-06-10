c5df4f088c006038382bd59e0698206c9a939a43
himansh231104 <him123ans456hu23@gmail.com>
2025-06-10 11:07:18 +0530

    API Key Update

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api`, 
-  // baseURL: `http://localhost:5000/api`, 
+  // baseURL: `${process.env.REACT_APP_API_URL}/api`, 
+  baseURL: `http://localhost:5000/api`, 
   headers: {
     'Content-Type': 'application/json',
   },
